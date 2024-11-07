f065d9925e2b49ba1bf622ba7ab17e21a8315a65
PaoHenao20 <paolaahenao7@gmail.com>
2024-11-06 22:12:11 -0500

    Entregar examen 1

--- examen_1/src/App.jsx
+++ examen_1/src/App.jsx
@@ -1,15 +1,59 @@
+import { useState } from "react";
 import Card from "./Card";
 
-
 function App() {
+
+  const [nombre, setNombre] = useState("");
+  const [libro, setLibro] = useState("");
+  const [error, setError] = useState("");
+  const [esValido, setEsValido] = useState(false);
+
+  const regexEspacioInicio = /^[^\s].*/;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      nombre.length >= 3 &&
+      regexEspacioInicio.test(nombre) &&
+      libro.length >= 6
+    ) {
+      setError("");
+      setEsValido(true);
+    } else {
+      setError("Por favor chequea que la información sea correcta");
+      setEsValido(false);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Elige un libro</h1>
+      <div className="form-content">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="nombre"
+            placeholder="Ingresa tu nombre"
+            className="color-form-input"
+            onChange={(e) => setNombre(e.target.value)}
+          />
+
+          <input
+            type="text"
+            id="libro"
+            placeholder="Ingresa tu libro favorito"
+            className="color-form-input"
+            onChange={(e) => setLibro(e.target.value)}
+          />
+          <button type="submit" className="color-form-button">
+            Submit!
+          </button>
+        </form>
+      </div>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {esValido && <Card nombre={nombre} libro={libro} />}
     </div>
   );
 }
-
 
 export default App;
--- examen_1/src/Card.jsx
+++ examen_1/src/Card.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({ nombre, libro }) => {
+  return (
+    <div>
+      <p>Hola {nombre}!</p>
+      <p>Sabemos que tu libro favorito es:</p>
+      <div>
+        {libro}
+      </div>
+    </div>
+  );
 };
-
 
 export default Card;
